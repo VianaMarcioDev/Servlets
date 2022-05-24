6f8e2b8882200fc2596d0678a176aea68f001118
Márcio de Freitas Viana <83033976+VianaMarcioDev@users.noreply.github.com>
2022-05-23 23:21:03 -0300

    Create get method to list all itens of Empresas class

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Empresa.java
@@ -1,5 +1,24 @@
 package br.com.alura.gerenciador.servlet;
 
 public class Empresa {
+	
+	private Integer id;
+	private String nome;
+	
+	public integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
 
 }
